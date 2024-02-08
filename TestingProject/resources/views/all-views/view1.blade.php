d61a3ecdc0e8701fd5ad42e0e00ffcb53721baa0
Iraklis-S <122298794+Iraklis-S@users.noreply.github.com>
2024-02-08 01:23:56 +0100

    removed wrong folder that was put in the project

--- TestingProject/resources/views/all-views/view1.blade.php
+++
@@ -1,7 +0,0 @@
-
-@extends('all-views.layout-view')
-
-@section('content')
-    <h2>VIEW1</h2>
-    <!-- Add home page content here -->
-@endsection
--- TestingProject/resources/views/all-views/view3.blade.php
+++
@@ -1,7 +0,0 @@
-
-@extends('all-views.layout-view')
-
-@section('content')
-    <h2>VIEW3</h2>
-    <!-- Add home page content here -->
-@endsection
